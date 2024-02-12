8ea81e2ebf7367fd57b12ebbc986fe375062ac8a
marametro <marametro@gmail.com>
2024-02-12 03:43:33 +0000

    Add Get Sum

---
+++ Udemy/getSum.js
@@ -0,0 +1,10 @@
+/*
+  Tulis sebuah function dengan nama `getSum` dengan 2 parameter `num1` dan `num2` 
+  Return pengembalian dari hasil penjumlahan `num1` dan `num2`
+*/
+
+function getSum(num1, num2)  {
+  return num1 + num2;
+}
+
+console.log(getSum(1,2))  
